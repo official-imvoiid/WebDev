9f9e16c157283777b49a374e878ac18101f86110
official-imvoiid <official.imvoiid@gmail.com>
2025-03-11 05:39:53 +0530

    Portfolio

--- Voiid/voiid/eslint.config.js
+++ Voiid/voiid/eslint.config.js
@@ -1,8 +1,7 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -17,22 +16,29 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
     plugins: {
-      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-]
+  {
+    // ESLint config specifically for tailwind.config.js
+    files: ['tailwind.config.js'],
+    env: {
+      node: true,  // Enable Node.js environment
+    },
+    rules: {
+      // Disable 'no-undef' for this file since `module` is a Node.js global
+      'no-undef': 'off',
+    },
+  },
+];
---
+++ Voiid/voiid/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Add your React file extensions here
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
